fc3a817cda24dcb69f82a463de439c8edc327c9d
matthew <mar17132@byui.edu>
2019-01-13 16:20:26 -0700

    update assignments

--- web/assignments/index.php
+++ web/assignments/index.php
@@ -6,21 +6,35 @@
 
 ?>
 
+
 <!DOCTYPE HTML>
 
 <html lang="en" >
     <head>
         <meta charset="utf-8" />
         <title>CS 313 Assignments</title>
-        <style type="text/css">
-
-        </style>    
+        <link rel="stylesheet" href="../style/introduction.css" />
+        <link rel="stylesheet" href="../style/intro-menu.css" />  
+        <script src="../scripts/jquery/jquery-3.3.1.min.js" ></script>
     </head>
     <body>
+        <div class="header">
+            <h1 class="title">CS 313 Assignments</h1>
+            <?php include '../documents/intro-menu.php';  ?>
+        </div>
 
-        <h1>Comming Soon</h1>
-        <script></script>
+        <div class="content">
+            
+        </div>
+
+        <div class="footer">
+
+        </div>        
+
+        <script type="text/javascript" src="../scripts/intro.js"> </script>
     </body>
 </html>
+
+
 
 
