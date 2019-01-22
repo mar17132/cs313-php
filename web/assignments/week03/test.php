6889a10ec033ae952dc4642183b45896a48e91cb
mar17132 <mar17132@byui.edu>
2019-01-22 15:57:11 -0700

    tes4

--- web/assignments/week03/test.php
+++ web/assignments/week03/test.php
@@ -3,7 +3,7 @@
 $baseURL = "https://enigmatic-lowlands-70024.herokuapp.com";
 $location = "/assignments/week03";
 
-include "/scripts/items.array.script.php";
+include "scripts/items.array.script.php";
 
 if(isset($items))
 {
