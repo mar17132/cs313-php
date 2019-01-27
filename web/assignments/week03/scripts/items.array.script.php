ae7f883852ea03424c5ce962266ad4bde4656388
mar17132 <mar17132@byui.edu>
2019-01-26 19:00:51 -0700

    dfg

--- web/assignments/week03/scripts/items.array.script.php
+++ web/assignments/week03/scripts/items.array.script.php
@@ -10,27 +10,27 @@
         ),
         2 =>array(
             "itemName" => "Frosty",
-            "itemImage" => "$baseURL$location/images/wendysjrfrosty.jpg",
+            "itemImage" => "$baseURL$location/images/wendysjrfrosty.png",
             "itemPrice" => "$125.00"
         ),
         3 =>array(
             "itemName" => "Proton Pack",
-            "itemImage" => "$baseURL$location/images/protonpackqx9.jpg",
+            "itemImage" => "$baseURL$location/images/protonpackqx9.png",
             "itemPrice" => "$950.00"
         ),
         4 =>array(
             "itemName" => "Whopper",
-            "itemImage" => "$baseURL$location/images/whoppercoin.jpeg",
+            "itemImage" => "$baseURL$location/images/whoppercoin.png",
             "itemPrice" => "$50.00"
         ),
         5 =>array(
             "itemName" => "WOPR",
-            "itemImage" => "$baseURL$location/images/WOPR-SideView.jpg",
+            "itemImage" => "$baseURL$location/images/WOPR-SideView.png",
             "itemPrice" => "$100,000,000.00"
         ),
         6 =>array(
             "itemName" => "Time Machine",
-            "itemImage" => "$baseURL$location/images/time.jpg",
+            "itemImage" => "$baseURL$location/images/time.png",
             "itemPrice" => "$100,000.00"
         )
 
