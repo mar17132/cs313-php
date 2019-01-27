5ca2ca0738a75e5af767fbe79cecb0e05a8159a6
mar17132 <mar17132@byui.edu>
2019-01-26 18:49:01 -0700

    lkk

--- web/assignments/week03/scripts/items.array.script.php
+++ web/assignments/week03/scripts/items.array.script.php
@@ -7,11 +7,37 @@
             "itemName" => "The Wrath of Khan",
             "itemImage" => "$baseURL$location/images/wrath.jpg",
             "itemPrice" => "$25.00"
+        ),
+        2 =>array(
+            "itemName" => "Frosty",
+            "itemImage" => "$baseURL$location/images/wendysjrfrosty.jpg",
+            "itemPrice" => "$125.00"
+        ),
+        3 =>array(
+            "itemName" => "Proton Pack",
+            "itemImage" => "$baseURL$location/images/protonpackqx9.jpg",
+            "itemPrice" => "$950.00"
+        ),
+        4 =>array(
+            "itemName" => "Whopper",
+            "itemImage" => "$baseURL$location/images/whoppercoin.jpeg",
+            "itemPrice" => "$50.00"
+        ),
+        5 =>array(
+            "itemName" => "WOPR",
+            "itemImage" => "$baseURL$location/images/WOPR-SideView.jpg",
+            "itemPrice" => "$100,000,000.00"
+        ),
+        6 =>array(
+            "itemName" => "Time Machine",
+            "itemImage" => "$baseURL$location/images/time.jpg",
+            "itemPrice" => "$100,000.00"
         )
+
+
 
     );
 
-//print_r($items);
 ?>
 
 
