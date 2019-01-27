14dde378b743bd41f9799010e6ce12383bb06377
mar17132 <mar17132@byui.edu>
2019-01-26 18:27:15 -0700

    gg

--- web/assignments/week03/documents/header-confirmation.php
+++ web/assignments/week03/documents/header-confirmation.php
@@ -8,6 +8,7 @@
 
 <?php include "head.php"; ?>
 <body>
+    <link rel="stylesheet" href="../style/confirmation.css"/>
     <iframe class="hiddenIframe" ></iframe>
     <div class="header">
         <h1 class="title">CS 313 Shopping Confirmation</h1>
