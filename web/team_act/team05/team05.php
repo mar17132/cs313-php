e93064a90d3ee5cb8d5aa08acde01056c0268a30
mar17132 <mar17132@byui.edu>
2019-02-02 09:16:31 -0700

    team05

---
+++ web/team_act/team05/team05.php
@@ -0,0 +1,56 @@
+<?php
+
+try
+{
+    $dbUrl = getenv('postgres://slslbiexbctcag:1e01412a806ed5a1cec30a5c99a6a784333ecd1c5b1ae5ec12b988069918ea53@ec2-107-22-162-8.compute-1.amazonaws.com:5432/dap2vvqc2qoa99');
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+?>
+
+
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Team 05</title>
+    </head>
+    <body>
+        <h1>Scripture Resources</h1>
+
+        <?php
+
+            foreach($db->query('SELECT * FROM Scriptures') as $row)
+            {
+                echo "<p>";
+                //scripture
+                echo "<span class='scripture'>";
+                echo $row['book']." ".$row['chapter'].":"$row['verses']." - ";
+                echo "</span>";
+                echo "&quot;".$row['content']."&quot;";
+                echo "</p>";
+
+            }
+
+
+        ?>
+
+    </body>
+</html>
+
