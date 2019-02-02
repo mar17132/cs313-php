da4369d61b5f11d9e7c85641aab0e5caa90b2d9e
mar17132 <mar17132@byui.edu>
2019-02-02 09:30:32 -0700

    hhh

--- web/team_act/team05/team05.php
+++ web/team_act/team05/team05.php
@@ -2,8 +2,9 @@
 
 //$db;
 echo " test2";
-/*try
+try
 {
+    echo " test3";
     $dbUrl = getenv('postgres://slslbiexbctcag:1e01412a806ed5a1cec30a5c99a6a784333ecd1c5b1ae5ec12b988069918ea53@ec2-107-22-162-8.compute-1.amazonaws.com:5432/dap2vvqc2qoa99');
     $dbOpts = parse_url($dbUrl);
 
@@ -22,7 +23,7 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-*/
+
 
 ?>
 
