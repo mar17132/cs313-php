b97f4999da8a706500d51b0c28f59835a6c3d743
mar17132 <mar17132@byui.edu>
2019-02-09 10:58:46 -0700

    ajaxs

---
+++ web/team_act/team06/display.ajax.php
@@ -0,0 +1,83 @@
+<?php
+
+$book = $_POST['book'];
+$chaper = $_POST['chapter'];
+$verse = $_POST['verse'];
+$content = $_POST['content'];
+$topics = $_POST['topic'];
+
+
+
+$db;
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+//insert new scripture
+$db->query("INSERT INTO Scriptures(book,chapter,verse,content)
+            VALUES('$book','$chaper','$verse','$content');");
+
+$scripturID = $db->lastInsertId('Scriptures_id_seq');
+
+//insert Scriptures_to_Topic
+
+foreach($topics as $topic)
+{
+    $db->query("INSERT INTO Scriptures_to_Topic(Scriptures_id,Topic_id)
+                VALUES($scripturID,$topic);");
+}
+
+
+$javascripObj = "{'topics':[";
+
+$statement = $db->query("SELECT * FROM Topic;");
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$resultsCount = count($results);
+
+foreach($results as $key => $row)
+{
+    $javascripObj .= "{'topic':$row,"
+
+    foreach($db->query("SELECT Scriptures_to_Topic.Scriptures_id,
+                        Scriptures.book,Scriptures.chapter,
+                        Scriptures.verse,Scriptures.content
+                        FROM Scriptures_to_Topic
+                        JOIN Scriptures
+                        ON Scriptures_to_Topic.Scriptures_id = Scriptures.ID
+                        WHERE Scriptures_to_Topic.Topic_id = $row[id];") as $scriptur)
+    {
+        $javascripObj .= "'book':$scriptur[book],'chapter':$scriptur[chapter],";
+        $javascripObj .= "'verse':$scriptur[verse],'content'$scriptur[content]}";
+    }
+
+    if($key < ($resultsCount - 1))
+    {
+        $javascripObj .= ",";
+    }
+}
+
+$javascripObj = "]}";
+
+echo $javascripObj;
+
+?>
+
---
+++ web/team_act/team06/team06.ajax.php
@@ -0,0 +1,140 @@
+<?php
+
+$db;
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Team 06</title>
+        <style type="text/css">
+            body{
+                font-size: 1em;
+            }
+            ul{
+                list-style-type: none;
+            }
+            .scripture{
+                font-weight: bold;
+            }
+        </style>
+        <script src="../../scripts/jquery/jquery-3.3.1.min.js" ></script>
+    </head>
+    <body>
+        <h1>Add a Scripture</h1>
+
+        <form method="post" action="display.php">
+
+            <label>Book</label>
+            <input type="text" name="book" />
+            <br/>
+
+            <label>Chapter</label>
+            <input type="text" name="chapter"/>
+            <br/>
+
+            <label>Verse</label>
+            <input type="text" name="verse"/>
+            <br/>
+
+            <label>Content</label>
+            <br/>
+            <textarea name="content" ></textarea>
+
+            <ul>
+                <li>Select Topics</li>
+            <?php
+
+              foreach($db->query('SELECT * FROM Topic;') as $row)
+                {
+
+                    echo "<li>";
+                    //scripture
+                    echo "<input type='checkbox' name='topic[]' value='".$row[id]."'/>";
+                    echo $row[name];
+                    echo "</li>";
+
+                }
+
+            ?>
+            </ul>
+            <br/>
+            <input type="submit" value="add scripture"/>
+        </form>
+
+        <div id="show">
+
+        </div>
+
+        <script type="text/javascript" >
+
+            function showScriptures(elemId, scriptureArray)
+            {
+
+                $.each(scriptureArray,function(index,value){
+                    scriptureArray
+                    parentElm = $("p");
+                    childElm = $("span");
+                    childElm.addClass('scripture');
+                    childElm.text(value.book + " " + value.chapter + ":" + value.verse + " - ");
+                    parentElm.appendTo(childElm);
+                    parentElm.text("&quot;" + value.content + "&quot;");
+
+                    elemId.appendTo(parentElm);
+
+                });
+
+            }
+
+
+            function showTopic(elemID,topic)
+            {
+                parentElm = $("h2");
+                parentElm.text(topic);
+                elemID.appendTo(parentElm);
+            }
+
+
+            function getScripturs(str)
+            {
+
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200)
+                    {
+                        document.getElementById("show").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET", "display.ajax.php", true);
+                xmlhttp.send();
+
+            }
+
+        </script>
+
+    </body>
+</html>
