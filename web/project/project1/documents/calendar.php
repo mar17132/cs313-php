4309711fb8fdfd059511291d738594538c2c07a9
mar17132 <mar17132@byui.edu>
2019-02-09 12:36:51 -0700

    asdsad

--- web/project/project1/documents/calendar.php
+++ web/project/project1/documents/calendar.php
@@ -4,10 +4,93 @@
 ?>
 
 
+
 <?php include "header-docs.php"; ?>
 
-        <div class="content">
+       <div class="content">
+            <h3>Scheduled Patches</h3>
+            <div class="table-div">
+                <ul class="table-row">
+                    <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Name
+                        </div>
+                    </li>
+                    <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Date
+                        </div>
+                    </li>
+                    <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Time
+                        </div>
+                    </li>
+                    <!--<li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Buttons
+                        </div>
+                    </li>-->
+                </ul>
 
+            <?php
+
+
+            $statement = $db->query("SELECT PatchSchedlue.ID AS scheduleID,
+                                PatchSchedlue.patchdate,PatchSchedlue.patchtime,
+                                PatchCycle.Name,PatchCycle.ID AS patchID
+                                FROM PatchSchedlue
+                                JOIN PatchCycle ON
+                                PatchSchedlue.PatchCycle_ID = PatchCycle.ID
+                                ORDER BY PatchSchedlue.patchdate;");
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+            if(count($result) > 0)
+            {
+                foreach($result as $row)
+                {
+                    echo "<ul class='table-row'>";
+
+                    //Name
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo $row[name];
+                    echo "</div>";
+                    echo "</li>\r\n";
+
+                    //Date
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo $row[patchdate];
+                    echo "</div>";
+                    echo "</li>\r\n";
+
+                    //Time
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo $row[patchtime];
+                    echo "</div>";
+                    echo "</li>\r\n";
+
+                    //Button
+                  /*  echo "<li class='table-cell'>
+                         <div class='table-cell-content'>";
+                    echo "No current Patch Cycles";
+                    echo "</div></li>";*/
+
+                    echo "</ul>";
+                }
+            }
+            else
+            {
+                echo "<ul class='table-row'><li class='table-cell'>
+                     <div class='table-cell-content'>";
+                echo "No current Patch Cycles";
+                echo "</div></li></ul>";
+            }
+
+            ?>
 
             <!-- Table elements
             <div class="table-div">
@@ -20,6 +103,7 @@
                 </ul>
             </div>-->
 
+            </div>
         </div>
 
         <div class="footer">
@@ -27,4 +111,5 @@
         </div>
     </body>
 </html>
+
 
--- web/project/project1/documents/servers.php
+++ web/project/project1/documents/servers.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 ?>
 
 
@@ -9,7 +8,81 @@
 <?php include "header-docs.php"; ?>
 
         <div class="content">
+            <h3>Computers</h3>
+            <div class="table-div">
+                <ul class="table-row">
+                    <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Name
+                        </div>
+                    </li>
+                    <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            IP
+                        </div>
+                    </li>
+                    <!--<li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Buttons
+                        </div>
+                    </li>-->
+                </ul>
 
+            <?php
+
+            $statement = "";
+            $result = "";
+
+            if(isset($_GET['serverID']))
+            {
+                $statement = $db->query("SELECT * FROM Computers WHERE ID ='".$_GET['serverID']."';");
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            }
+            else
+            {
+                $statement = $db->query("SELECT * FROM Computers;");
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+
+            if(count($result) > 0)
+            {
+                foreach($result as $row)
+                {
+                    echo "<ul class='table-row'>";
+
+                    //Name
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo $row[name];
+                    echo "</div>";
+                    echo "</li>\r\n";
+
+                    //Notes
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo $row[ip];
+                    echo "</div>";
+                    echo "</li>\r\n";
+
+                    //Button
+                  /*  echo "<li class='table-cell'>
+                         <div class='table-cell-content'>";
+                    echo "No current Patch Cycles";
+                    echo "</div></li>";*/
+
+                    echo "</ul>";
+                }
+            }
+            else
+            {
+                echo "<ul class='table-row'><li class='table-cell'>
+                     <div class='table-cell-content'>";
+                echo "No current Patch Cycles";
+                echo "</div></li></ul>";
+            }
+
+            ?>
 
             <!-- Table elements
             <div class="table-div">
@@ -22,6 +95,7 @@
                 </ul>
             </div>-->
 
+            </div>
         </div>
 
         <div class="footer">
