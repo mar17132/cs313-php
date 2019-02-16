40b3702f8504e27002f64da27c0a239798172caa
mar17132 <mar17132@byui.edu>
2019-02-16 09:09:17 -0700

    h

--- web/team_act/team07/sign-in.php
+++ web/team_act/team07/sign-in.php
@@ -41,11 +41,12 @@ $errorMess = "";
 
 if(count($_POST) > 0)
 {
-    print_r($_POST);
-  /*  $statement = $db->query("SELECT * FROM Users7 WHERE name='".$_POST['usernameTxt'].";");
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    if(count($results) > 0)
+    $statement = $db->query("SELECT * FROM Users7 WHERE name='".$_POST['usernameTxt'].";");
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+print_r($results);
+    echo count($results);
+   /* if(count($results) > 0)
     {
         if(password_verify($_POST['passTxt'],$results[pass]))
         {
@@ -60,8 +61,8 @@ if(count($_POST) > 0)
     else
     {
         $errorMess ="Username or Password is incorrect";
-    }
-*/
+    }*/
+
 }
 
 ?>
