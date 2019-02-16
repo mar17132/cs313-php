2048d89240ff66099ce4a0332f0020c7e8af1263
mar17132 <mar17132@byui.edu>
2019-02-16 12:42:56 -0700

    dd

--- web/project/project1/documents/servers.php
+++ web/project/project1/documents/servers.php
@@ -12,9 +12,14 @@
 
             <div class="table-div">
                 <ul class="table-row">
+                        li class="table-cell">
+                        <div class="table-cell-content">
+                           &nbsp;
+                        </div>
+                    </li>
                     <li class="table-cell">
                         <div class="table-cell-content">
-                           <a href="addserver.php">
+                           <a href="addserver.php" class="addContent">
                                 <input type="button" value="Add Server" />
                             </a>
                         </div>
