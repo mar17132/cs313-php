5e128c1ec4625697db73f75d999a7fa9c1f8ed4d
mar17132 <mar17132@byui.edu>
2019-02-16 09:20:51 -0700

    ff

--- web/team_act/team07/sign-in.php
+++ web/team_act/team07/sign-in.php
@@ -42,10 +42,10 @@ $errorMess = "";
 if(count($_POST) > 0)
 {
 
-    $statement = $db->query("SELECT * FROM Users7 WHERE name='".$_POST['usernameTxt'].";");
-   // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-//print_r($results);
- //echo count($results);
+    $statement = $db->query("SELECT * FROM Users7 WHERE name='".$_POST[usernameTxt]."';");
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+print_r($results);
+ echo count($results);
    /* if(count($results) > 0)
     {
         if(password_verify($_POST['passTxt'],$results[pass]))
