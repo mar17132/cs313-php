ceccb965b39df5286daaf9df3cf74e229b8b9484
mar17132 <mar17132@byui.edu>
2019-02-16 12:37:25 -0700

    f

--- web/project/project1/documents/servers.php
+++ web/project/project1/documents/servers.php
@@ -9,8 +9,30 @@
 
         <div class="content">
             <h3>Computers</h3>
+
             <div class="table-div">
                 <ul class="table-row">
+                    <li class="table-cell">
+                        <div class="table-cell-content">
+                           <a href="addserver.php">
+                                <input type="button" value="Add Server" />
+                            </a>
+                        </div>
+                    </li>
+                    <li class="table-cell">
+                        <div class="table-cell-content">
+                           <form method="post" action="addserver.php">
+                                <input type="submit" value="Add Server" />
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="table-row">
+                     <li class="table-cell">
+                        <div class="table-cell-head-content">
+                            Select
+                        </div>
+                    </li>
                     <li class="table-cell">
                         <div class="table-cell-head-content">
                             Name
@@ -50,6 +72,15 @@
                 foreach($result as $row)
                 {
                     echo "<ul class='table-row'>";
+
+                    //Select
+                    echo "<li class='table-cell'>";
+                    echo "<div class='table-cell-content'>";
+                    echo "<input type='checkbox' class='selectValueChk' value='";
+                    echo $row[id];
+                    echo "'/>";
+                    echo "</div>";
+                    echo "</li>\r\n";
 
                     //Name
                     echo "<li class='table-cell'>";
@@ -98,9 +129,26 @@
             </div>
         </div>
 
-        <div class="footer">
+        <div class="footer" >
 
         </div>
+
+        <script>
+            var selectChk = $(".selectValueChk");
+
+            function disableAllCheck(elem)
+            {
+                elem.prop("disabled",true);
+            }
+
+            selectChk.on("click",function(){
+
+                disabledAllCheck(selectChk);
+                $(this).prop("disabled",false);
+
+            });
+
+        </script>
     </body>
 </html>
 
