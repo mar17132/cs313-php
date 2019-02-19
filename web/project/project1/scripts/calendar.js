099437c3750782df645e6b7fdc6a3ebf41ffb3df
mar17132 <mar17132@byui.edu>
2019-02-19 13:02:13 -0700

    xxx

--- web/project/project1/scripts/calendar.js
+++ web/project/project1/scripts/calendar.js
@@ -433,13 +433,11 @@ $(document).ready(function(){
         bulidCalendar();
     });
 
-    //jsonCalendarObj((showingMonth + 1), showingYear);
+    jsonCalendarObj((showingMonth + 1), showingYear);
 
-    //bulidCalendar();
-    $.when($.get("https://enigmatic-lowlands-70024.herokuapp.com/project/project1/scripts/getCalendarEvent.php",
-                {month:"2",year:"2019"}),function(data){
-        appointmentObj = JSON.parse(data);
-    }).done(bulidCalendar());
+
+    setTimeout(bulidCalendar(),1250);
+
 
 });
 
